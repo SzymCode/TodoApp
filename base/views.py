e1b02a1f7c183b1781ec4c309c6f22cb546d7e45
Pythadela <s.radomski19@gmail.com>
2023-01-29 19:11:29 +0100

    Task update + delete

--- base/views.py
+++ base/views.py
@@ -5,7 +5,7 @@ from django.contrib.auth.decorators import login_required
 from .forms import CreateUserForm
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Task
 
@@ -23,6 +23,17 @@ class TaskCreate(CreateView):
     model = Task
     fields = '__all__'
     success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+
 
 
 def registerPage(request):
