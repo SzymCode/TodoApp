dd1bb038ea31640bf29c73d5357040b3d0de8133
Pythadela <s.radomski19@gmail.com>
2023-01-29 19:09:48 +0100

    Task create + view details

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 from . import views
-from .views import TaskList
+from .views import TaskList, TaskDetail, TaskCreate
 
 urlpatterns = [
     path('register/', views.registerPage, name="register"),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutUser, name="logout"),
+
     path('', TaskList.as_view(), name="tasks"),
+    path('task/<int:pk>/', TaskDetail.as_view(), name="task"),
+    path('create-task/', TaskCreate.as_view(), name='task-create'),
 ]
