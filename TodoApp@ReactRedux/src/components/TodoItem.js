77615156a4562093e39ba6a5b5ff41f7cb3bbfba
SzymCode <s.radomski19@gmail.com>
2023-06-06 11:28:23 +0200

    Delete items

--- TodoApp@ReactRedux/src/components/TodoItem.js
+++ TodoApp@ReactRedux/src/components/TodoItem.js
@@ -1,21 +1,38 @@
+import toast from 'react-hot-toast';
 import React from 'react';
+import { MdDelete } from 'react-icons/md';
+import { useDispatch } from 'react-redux';
+import { deleteTodo } from '../slices/todoSlice';
+
+
 
 function TodoItem({ todo }) {
+  const dispatch = useDispatch();
+
+
+  const handleDelete = () => {
+    dispatch(deleteTodo(todo.id));
+    toast.success('Todo Deleted Successfully');
+  };
+
+
   return (
-    <div key={ todo.id } className="todoItem">
-      <p>
-        { todo.title }
-      </p>
-      <p>
-        { todo.description }
-      </p>
-      <p>
-        { todo.status }
-      </p>
-      <p>
-        { todo.time }
-      </p>
-    </div>
+    <>
+      <div>
+        <p>
+          {todo.title}
+        </p>
+        <p>
+          {todo.description}
+        </p>
+        <p>
+          {todo.time}
+        </p>
+      </div>
+      <div onClick={() => handleDelete()} onKeyDown={() => handleDelete()} tabIndex={0} role="button">
+        <MdDelete />
+      </div>
+    </>
   );
 }
 
